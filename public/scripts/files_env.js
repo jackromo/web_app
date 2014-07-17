ae7ea5b2373bb92ef44570019bdd71529de239b0
jackromo <sharrackor@gmail.com>
2014-07-16 20:00:23 -0400

    Allow viewing of user's programs in /files

---
+++ public/scripts/files_env.js
@@ -0,0 +1,34 @@
+//This file provides the code behind the file editor environments.
+//It assumes the existence of a canvas (mycanvas), a div (output) and a textfield (yourcode).
+//The canvas is used for graphics, and the div for text.
+
+//This returns an output function which works on the element 'out_pre'.
+function outf(out_pre) {
+  return function(text) {
+    out_pre.innerHTML = out_pre.innerHTML + text;
+  }
+}
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+//This will run the i-th textarea's code in its respective canvas and pre.
+//It takes the index of the textarea (i) as input, and grabs all the associated HTML elements.
+//It then configures the output function, and calls Sk.importMainWithBody().
+function runit(i) {
+   var prog = document.getElementById("code_" + i).value;
+   var canvas = document.getElementById("canvas_" + i);
+   var out_pre = document.getElementById("output_" + i);
+   out_pre.innerHTML = '';
+   Sk.canvas = "canvas_" + i;
+   Sk.pre = "output_" + i;
+   Sk.configure({output:outf(out_pre), read:builtinRead});
+   try {
+      eval(Sk.importMainWithBody("<stdin>",false,prog));
+   }
+   catch(e) {
+       alert(e.toString())
+   }
+}
